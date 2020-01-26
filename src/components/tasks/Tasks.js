123b121bf6acc6e82923049b905d12caf0f6b108
Guilherme Barbosa <guilhermebarbosa@Guilhermes-MacBook-Pro.local>
2020-01-26 15:59:33 -0300

    buscando dados do rails

--- src/components/tasks/Tasks.js
+++ src/components/tasks/Tasks.js
@@ -1,23 +1,44 @@
 import React, { Component } from 'react';
    import Row from 'react-bootstrap/Row';
    import Col from 'react-bootstrap/Col';
-  //  import List from '../list/List';
+   import List from '../tasks/List';
    
    class Tasks extends Component {
-     render() {
-       return (
-         <Row>
-           <Col xs={{ span: 8, offset: 2 }} className="tasks_list">
-             <p className="title">To-do</p>
-             {/* <List/> */}
-           </Col>
-           <Col xs={{ span: 8, offset: 2 }} className="tasks_list">
-             <p className="title">Done</p>
-             {/* <List/> */}
-           </Col>
-         </Row>
-       );
-     }
+
+    constructor(props) {
+      super(props);
+      this.state = {
+        tasks: []
+      };
+      this.loadTasks = this.loadTasks.bind(this);
+    }
+
+    async loadTasks() {
+      let response = await fetch(`http://0.0.0.0:3001/tasks`);
+      
+      const tasks = await response.json();
+      this.setState({ tasks: tasks });
+    }
+
+    componentDidMount() {
+      this.loadTasks();
+    }
+
+    render() {
+      return (
+        <Row>
+          <Col xs={{ span: 8, offset: 2 }} className="tasks_list">
+            <p className="title">To-do</p>
+            <List tasks={this.state.tasks.filter((task) => task.done != true)}/>
+          </Col>
+          <Col xs={{ span: 8, offset: 2 }} className="tasks_list">
+            <p className="title">Done</p>
+            <List tasks={this.state.tasks.filter((task) => task.done == true)}/>
+          </Col>
+        </Row>
+      );
+    }
+
    }
    
    export default Tasks;
